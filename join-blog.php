37422bd0d28e6fc1f1e2a41ecd5a02f885c0234f
Brajesh Singh <brajesh@cosmiccoders.com>
2018-03-02 10:51:56 +0530

    allow admins to show register link to visitors

--- join-blog.php
+++ join-blog.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Join Blog Widget
- * Version: 1.0.2
+ * Version: 1.0.3
  * Plugin URI: https://buddydev.com/plugins/join-blog-widget/
  * Author: BuddyDev
  * Author URI: https://buddydev.com
@@ -38,10 +38,17 @@ class BPDevJoinBlogWidget extends WP_Widget {
 		$user_id = get_current_user_id();
 		$blog_id = get_current_blog_id();
 
-		// if the user is not logged in or the user is already a member of the blog, do not show this widget.
-		if ( empty( $user_id ) || is_user_member_of_blog( $user_id, $blog_id ) ) {
-			return false;
-		}
+		$show = false;
+
+		if( ! $user_id && ! empty( $instance['show_to_non_logged'] ) ) {
+		    $show = true;
+        } elseif( ! is_user_member_of_blog( $user_id, $blog_id ) ) {
+		    $show = true;
+        }
+
+        if( ! $show ) {
+		    return ;
+        }
 
 		// display the widget with button to ask joining.
 		echo $args['before_widget'];
@@ -49,10 +56,40 @@ class BPDevJoinBlogWidget extends WP_Widget {
 			echo $args['before_title'] . $instance['title'] . $args['after_title'];
 		}
 
-		echo "<a data-id='{$this->id}' class='bpdev-join-blog' href='" . get_blogaddress_by_id( $blog_id ) . "?action=join-blog&_wpnonce=" . wp_create_nonce( 'join-blog-' . $this->id ) . "'>" . $instance['button_text'] . "</a>";
-		echo $args['after_widget'];
+		if( is_user_logged_in() ) {
+		    $this->content_for_loggedin_members( $instance );
+        } else {
+		    $this->content_non_members( $instance );
+        }
 
+		echo $args['after_widget'];
 	}
+
+	/**
+     * Message for members.
+     *
+	 * @param $instance
+	 */
+	private function content_for_loggedin_members( $instance ) {
+	    $blog_id = get_current_blog_id();
+		$url = add_query_arg(
+			array(
+				'action'   => 'join-blog',
+				'_wpnonce' => wp_create_nonce( 'join-blog-' . $this->id )
+			), get_blogaddress_by_id( $blog_id ) );
+
+		echo "<a data-id='{$this->id}' class='bpdev-join-blog' href='{$url}'>{$instance['button_text']}</a>";
+    }
+
+	/**
+     * Message for non members.
+     *
+	 * @param $instance
+	 */
+	private function content_non_members( $instance ) {
+	    $message = isset( $instance['non_logged_message'] ) ? $instance['non_logged_message'] : '';
+	   echo $message;
+    }
 
 	/**
      * Update widget settings
@@ -64,12 +101,14 @@ class BPDevJoinBlogWidget extends WP_Widget {
 	 */
 	public function update( $new_instance, $old_instance ) {
 
-		$instance                    = $old_instance;
-		$instance['title']           = $new_instance['title'];
-		$instance['button_text']     = $new_instance['button_text'];
-		$instance['message_success'] = $new_instance['message_success'];
-		$instance['message_error']   = $new_instance['message_error'];
-		$instance['role']            = $new_instance['role'];
+		$instance                       = $old_instance;
+		$instance['title']              = $new_instance['title'];
+		$instance['button_text']        = $new_instance['button_text'];
+		$instance['message_success']    = $new_instance['message_success'];
+		$instance['message_error']      = $new_instance['message_error'];
+		$instance['role']               = $new_instance['role'];
+		$instance['show_to_non_logged'] = isset( $new_instance['show_to_non_logged'] ) ? absint( $new_instance['show_to_non_logged'] ) : 0;
+		$instance['non_logged_message'] = isset( $new_instance['non_logged_message'] ) ? $new_instance['non_logged_message'] : '';
 
 		return $instance;
 	}
@@ -78,13 +117,18 @@ class BPDevJoinBlogWidget extends WP_Widget {
      * Display widget settings form
 	 */
 	public function form( $instance ) {
-		$default = array( 'title'           => _x( 'Join Blog', 'Widget title', 'join-blog-widget' ),
-		                  'role'            => 'subscriber',
-		                  'button_text'     => _x( 'Join this Blog', 'button label', 'join-blog-widget' ),
-		                  'message_success' => _x( 'You have successfully joined this blog.', 'success message on joining', 'join-blog-widget' ),
-		                  'message_error'   => _x( 'There was a problem joining this blog. Please try again later.', 'Failure message for unable to join', 'join-blog-widget' )
+		$default = array(
+			'title'              => _x( 'Join Blog', 'Widget title', 'join-blog-widget' ),
+			'role'               => 'subscriber',
+			'button_text'        => _x( 'Join this Blog', 'button label', 'join-blog-widget' ),
+			'message_success'    => _x( 'You have successfully joined this blog.', 'success message on joining', 'join-blog-widget' ),
+			'message_error'      => _x( 'There was a problem joining this blog. Please try again later.', 'Failure message for unable to join', 'join-blog-widget' ),
+			'show_to_non_logged' => 1,
+			'non_logged_message' => sprintf( __( 'Please create an account to join this site. <a href="%s">Signup Now</a>.', 'join-blog-widget' ), wp_registration_url() ),
 		);
 		$args    = wp_parse_args( (array) $instance, $default );
+		$show_to_non_logged = isset( $args['show_to_non_logged'] )? absint($args['show_to_non_logged'] ) : 0;
+		$non_logged_message = isset( $args['non_logged_message'] )? $args['non_logged_message'] : '';
 		?>
         <p>
             <label for="<?php $this->get_field_id( 'title' ); ?>"><?php _e( 'Title' ); ?><br/>
@@ -114,6 +158,19 @@ class BPDevJoinBlogWidget extends WP_Widget {
             <label for="<?php $this->get_field_id( 'message_error' ); ?>"> <?php _e( 'Error Message' ); ?>
                 <textarea id="<?php $this->get_field_id( 'message_error' ); ?>"
                           name="<?php echo $this->get_field_name( 'message_error' ); ?>" class="widefat"><?php echo $args['message_error']; ?></textarea>
+            </label>
+        </p>
+
+        <p>
+            <label for="<?php $this->get_field_id( 'show_to_non_logged' ); ?>"> <?php _e( 'Show to Not logged in users?' ); ?>
+                <input id="<?php $this->get_field_id( 'show_to_non_logged' ); ?>"
+                          name="<?php echo $this->get_field_name( 'show_to_non_logged' ); ?>" type="checkbox" value="1" <?php checked(1, $show_to_non_logged );?>
+            </label>
+        </p>
+        <p>
+            <label for="<?php $this->get_field_id( 'non_logged_message' ); ?>"> <?php _e( 'Message for Non Logged User' ); ?>
+                <textarea id="<?php $this->get_field_id( 'non_logged_message' ); ?>"
+                          name="<?php echo $this->get_field_name( 'non_logged_message' ); ?>" class="widefat"><?php echo esc_textarea($non_logged_message ); ?></textarea>
             </label>
         </p>
 
